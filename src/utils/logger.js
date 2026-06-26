81a7110eaf965cb9dc4b0d136d2e2c61c84f00d0
joaocapelari1-dot <joaocapelari1@gmail.com>
2026-06-26 10:19:16 -0300

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,34 +1,33 @@
-const chalk = require('chalk');
-
 class Logger {
   constructor(context = 'APP') {
     this.context = context;
   }
 
+  _time() {
+    return new Date().toISOString().split('T')[1].split('.')[0];
+  }
+
   _format(level, msg) {
-    const time = new Date().toISOString().split('T')[1].split('.')[0];
-    return `[${time}] [${level}] [${this.context}] ${msg}`;
+    return `[${this._time()}] [${level}] [${this.context}] ${msg}`;
   }
 
   info(msg) {
-    console.log(chalk.cyan(this._format('INFO ', msg)));
+    console.log(this._format('INFO ', msg));
   }
 
   warn(msg) {
-    console.log(chalk.yellow(this._format('WARN ', msg)));
+    console.log(this._format('WARN ', msg));
   }
 
   error(msg) {
-    console.log(chalk.red(this._format('ERROR', msg)));
+    console.log(this._format('ERROR', msg));
   }
 
   debug(msg) {
     if (process.env.DEBUG) {
-      console.log(chalk.gray(this._format('DEBUG', msg)));
+      console.log(this._format('DEBUG', msg));
     }
   }
 }
 
-// export compatível com require()
 module.exports = (context) => new Logger(context);
-module.exports.Logger = Logger;
