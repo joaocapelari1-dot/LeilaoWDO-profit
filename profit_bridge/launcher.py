b5ea759a529e2d57d21dd5d6e2977b78b96840e3
joaocapelari1-dot <joaocapelari1@gmail.com>
2026-06-29 07:30:24 -0300

    fix: RAILWAY_WS_URL /ws -> /bridge — bridge conectava na rota errada

--- profit_bridge/launcher.py
+++ profit_bridge/launcher.py
@@ -1,11 +1,11 @@
 import os, runpy
 
 os.environ['PROFIT_ACTIVATION_KEY'] = '2331312907888866889'
-os.environ['PROFIT_USERNAME'] = 'joaocapelari1@gmail.com'
-os.environ['PROFIT_PASSWORD'] = '321Angelin@@@'
-os.environ['PROFIT_DLL_PATH'] = r'C:\ProfitBridge\Win64\ProfitDLL.dll'
-os.environ['SYMBOLS'] = 'WDON26,DOLN26'
-os.environ['RAILWAY_WS_URL'] = 'wss://leilaowdo-profit-production.up.railway.app/ws'
-os.environ['BRIDGE_SECRET'] = '321Angelin@@'
+os.environ['PROFIT_USERNAME']       = 'joaocapelari1@gmail.com'
+os.environ['PROFIT_PASSWORD']       = '321Angelin@@@'
+os.environ['PROFIT_DLL_PATH']       = r'C:\ProfitBridge\Win64\ProfitDLL.dll'
+os.environ['SYMBOLS']               = 'WDON26,DOLN26'
+os.environ['RAILWAY_WS_URL']        = 'wss://leilaowdo-profit-production.up.railway.app/bridge'
+os.environ['BRIDGE_SECRET']         = '321Angelin@@'
 
 runpy.run_path(r'C:\ProfitBridge\watchdog.py', run_name='__main__')
