e76752f7d2b9f4f4df181820519e31300254650d
joaocapelari1-dot <joaocapelari1@gmail.com>
2026-06-30 07:59:23 -0300

    fix: atualizar simbolos para vencimento Q26 (agosto) — virada de mes 30/06

--- profit_bridge/launcher.py
+++ profit_bridge/launcher.py
@@ -4,7 +4,7 @@ os.environ['PROFIT_ACTIVATION_KEY'] = '2331312907888866889'
 os.environ['PROFIT_USERNAME']       = 'joaocapelari1@gmail.com'
 os.environ['PROFIT_PASSWORD']       = '321Angelin@@@'
 os.environ['PROFIT_DLL_PATH']       = r'C:\ProfitBridge\Win64\ProfitDLL.dll'
-os.environ['SYMBOLS']               = 'WDON26,DOLN26'
+os.environ['SYMBOLS']               = 'WDOQ26,DOLQ26'
 os.environ['RAILWAY_WS_URL']        = 'wss://leilaowdo-profit-production.up.railway.app/bridge'
 os.environ['BRIDGE_SECRET']         = '321Angelin@@'
 
